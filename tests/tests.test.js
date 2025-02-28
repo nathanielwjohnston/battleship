4dc852a4d2aeb08eecb4a63ad892e654af7c2bcc
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2025-02-28 23:10:05 +0000

    Fix createShip test bugs

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -5,17 +5,14 @@ describe("ship", () => {
 
   it("can be hit", () => {
     expect(ship.hit()).toBe(1);
-  })
-  for (let i=0; i < 4; i++) {
-    ship.hit();
-  }
+  });
   it("isn't always sunk", () => {
     expect(ship.isSunk()).toBe(false);
-  })
-  for (let i=0; i < 4; i++) {
-    ship.hit();
-  }
+  });
   it("can be sunk", () => {
+    for (let i = 0; i < 4; i++) {
+      ship.hit();
+    }
     expect(ship.isSunk()).toBe(true);
-  })
-})
+  });
+});
