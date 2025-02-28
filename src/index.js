8201cce27cf5e54122535c76fb032d4d26cb5168
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2025-02-28 23:11:25 +0000

    Write createShip functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "./styles.css";
 
 export function createShip(length) {
+  let timesHit = 0;
 
+  const hit = () => ++timesHit;
+
+  function isSunk() {
+    if (timesHit >= length) return true;
+
+    return false;
+  }
+
+  return { hit, isSunk };
 }
