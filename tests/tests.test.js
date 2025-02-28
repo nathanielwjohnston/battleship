b7b4f15800c0b9f4d6516b6b46517dcbdc00aeba
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2025-02-28 22:55:37 +0000

    Create basic testing group for createShip

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -1,1 +1,21 @@
 import { createShip } from "../src";
+
+describe("ship", () => {
+  const ship = createShip(5);
+
+  it("can be hit", () => {
+    expect(ship.hit()).toBe(1);
+  })
+  for (let i=0; i < 4; i++) {
+    ship.hit();
+  }
+  it("isn't always sunk", () => {
+    expect(ship.isSunk()).toBe(false);
+  })
+  for (let i=0; i < 4; i++) {
+    ship.hit();
+  }
+  it("can be sunk", () => {
+    expect(ship.isSunk()).toBe(true);
+  })
+})
