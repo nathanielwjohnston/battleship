73529b4f90a2c836f8b669bf440635fc634dc3c1
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2025-03-02 22:12:57 +0000

    gameboard: write functionality

--- src/index.js
+++ src/index.js
@@ -14,4 +14,46 @@ export function createShip(length) {
   return { hit, isSunk };
 }
 
-export function createGameboard() {}
+export function createGameboard() {
+  const ships = [];
+  const hits = [];
+  const misses = [];
+
+  function placeShip(coordinatesArray, length) {
+    const ship = createShip(length);
+    ships.push({ shipObject: ship, coordinatesArray });
+
+    return true;
+  }
+
+  function receiveAttack(coordinates) {
+    for (let ship of ships) {
+      const gridSquares = ship.coordinatesArray;
+      for (let gridSquare of gridSquares) {
+        if (
+          gridSquare[0] === coordinates[0] &&
+          gridSquare[1] === coordinates[1]
+        ) {
+          ship.shipObject.hit();
+          hits.push(coordinates);
+          // Remove coordinate from ship - is essentially moved over to hits
+          gridSquares.splice(gridSquares.indexOf(gridSquare), 1);
+          // Remove ship from ships
+          if (!gridSquares.length) {
+            ships.splice(ships.indexOf(ship), 1);
+          }
+          return true;
+        }
+      }
+    }
+
+    misses.push(coordinates);
+    return false;
+  }
+
+  function checkRemainingShips() {
+    return ships.length;
+  }
+
+  return { placeShip, receiveAttack, checkRemainingShips, misses, hits, ships };
+}
